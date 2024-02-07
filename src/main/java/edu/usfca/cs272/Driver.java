b67b951eb4a5285dc9f201351ba71c615bff70b7
evanalulu <evanapra@gmail.com>
2024-02-07 11:08:54 -0800

    printed out args lol

--- src/main/java/edu/usfca/cs272/Driver.java
+++ src/main/java/edu/usfca/cs272/Driver.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  * Class responsible for running this project based on the provided command-line
  * arguments. See the README for details.
  *
- * @author TODO Add Name Here
+ * @author Evana Pradhan
  * @author CS 272 Software Development (University of San Francisco)
  * @version Spring 2024
  */
@@ -25,15 +25,37 @@ public class Driver {
 		// store initial start time
 		Instant start = Instant.now();
 
-		// TODO Fill in and modify as needed
 		System.out.println("Working Directory: " + Path.of(".").toAbsolutePath().normalize().getFileName());
-		System.out.println("Arguments: " + Arrays.toString(args));
+//		System.out.println("Arguments: " + Arrays.toString(args));
+		
+		String textPath = null;
+        String countsPath = "counts.json";
+        
+        for (int i = 0; i < args.length; i ++) {
+        	if (args[i].equals("-text")) {
+        		if (args.length > i + 1) {
+        			textPath = args[i + 1];
+        			i++;
+        		} else {
+        			System.err.print("Error: Missing argument for -text");
+        			return;
+        		}
+            } else if (args[i].equals("-counts")) {
+            	if (args.length > i + 1) {
+            		countsPath = args[i + 1];
+            	} else {
+        			System.err.print("Error: Missing argument for -counts");
+        			return;
+        		}
+            	
+            }
+        }
+         
+		System.out.println("-text: " + textPath + " -count: " + countsPath);
 
-		// calculate time elapsed and output
-		long elapsed = Duration.between(start, Instant.now()).toMillis();
-		double seconds = (double) elapsed / Duration.ofSeconds(1).toMillis();
-		System.out.printf("Elapsed: %f seconds%n", seconds);
 	}
+	
+	
 
 	/*
 	 * Generally, "Driver" classes are responsible for setting up and calling other
