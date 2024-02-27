d710070621c16ad307f29b429e8b0b1f25b5dfe0
evanalulu <evanapra@gmail.com>
2024-02-26 20:20:16 -0800

    Implemented InvertedIndex type argument in FileProcessor

--- src/main/java/edu/usfca/cs272/FileProcessor.java
+++ src/main/java/edu/usfca/cs272/FileProcessor.java
@@ -11,23 +11,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
 
-import org.apache.commons.lang3.tuple.Pair;
-
-/*
- * TODO 
-
-Create 1 new "InvertedIndex" data structure class:
-
-Map<String, Integer> wordCountMap 
-TreeMap<String, TreeMap<String, ArrayList<Integer>>> indexMap
-
-at a minimum, add methods:
-
-addCount(String location, Integer count) --> wordCountMap
-addWord(String word, String location, Integer position) --> indexMap
-
- */
-
 /**
  * Class responsible for processing files and directories.
  * It includes methods to traverse directories recursively and read the content of text files,
@@ -46,42 +29,41 @@ public class FileProcessor {
 	 * @throws IOException if an I/O error occurs while traversing the directory or processing files
 	 */
 	public static void traverseDirectory(Path directory, InvertedIndex index) throws IOException {
-		
-	}
-	
-//	public static void traverseDirectory(Path directory, Map<String, Integer> wordCountMap, TreeMap<String, TreeMap<String, ArrayList<Integer>>> indexMap) throws IOException{
-//		try (DirectoryStream<Path> paths = Files.newDirectoryStream(directory)) {
-//			paths.forEach(path -> { // TODO for (Path path : paths)
-//				if (Files.isDirectory(path)) {
-//					try { // TODO Remove try/catch entirely
-//						traverseDirectory(path, wordCountMap, indexMap);
-//					} catch (IOException e) {
-//					}
-//				} else if (Files.isRegularFile(path) && isExtensionText(path)) {
-//                	Pair<Integer, TreeMap<String, TreeMap<String, ArrayList<Integer>>>> maps;
-//					try {
-//						maps = readFile(path);
-//						// TODO SHould only need the readFile call here
-//	                    int wordCount = maps.getLeft();
-//	                    if (wordCount > 0) 
-//	                    	wordCountMap.put(path.toString(), wordCount);
-//	                    
-//	                    TreeMap<String, TreeMap<String, ArrayList<Integer>>> index = maps.getRight();
-//	                    
-//	                    // TODO Use for loop
-//	                    index.forEach((word, filePathToIndicesMap) ->
-//	                    filePathToIndicesMap.forEach((filePath, indices) ->
-//	                        indexMap.computeIfAbsent(word, k -> new TreeMap<>())
-//	                                .put(filePath, new ArrayList<>(indices))
-//	                    	)
-//	                    );
-//					} catch (IOException e) {
-//						// TODO Clue you are catching in the wrong place
-//					}
-//				}
-//			});
-//		}
-//	}
+        try (DirectoryStream<Path> paths = Files.newDirectoryStream(directory)) {
+            for (Path path : paths) {
+                if (Files.isDirectory(path)) {
+                    traverseDirectory(path, index);
+                } else if (Files.isRegularFile(path) && isExtensionText(path)) {
+                    InvertedIndex maps;
+                    try {
+                        readFile(path, index);
+                        int wordCount = index.getWordCountMap().get(path);
+                        
+                        TreeMap<String, TreeMap<String, ArrayList<Integer>>> indexMap = index.getIndexMap();
+
+                        for (Map.Entry<String, TreeMap<String, ArrayList<Integer>>> entry : indexMap.entrySet()) { 
+	                        String key = entry.getKey();
+	                        TreeMap<String, ArrayList<Integer>> value = entry.getValue();
+	                        
+	                        String filePath = value.keySet().iterator().next();
+	                        ArrayList<Integer> indices = value.get(filePath); 
+	                        
+	                        if (!indexMap.containsKey(key)) {
+	                        	TreeMap<String, ArrayList<Integer>> temp = new TreeMap<>();
+	                        	indexMap.put(key, temp);
+	                        	indexMap.get(key).put(filePath, indices);
+	                        } else {
+	                        	indexMap.get(key).put(filePath, indices);
+	                        }
+	                        
+	                    } 
+                    } catch (IOException e) {
+                        // 
+                    }
+                }
+            }
+        }
+    }
 
 	/**
 	 * Reads the content of the file located at the specified path, processes the words, and returns
@@ -91,13 +73,13 @@ public class FileProcessor {
 	 * @return a pair containing the word count and a map of word positions
 	 * @throws IOException if an I/O error occurs while reading the file
 	 */
-	public static InvertedIndex readFile(Path path, InvertedIndex index) throws IOException {
+	public static void readFile(Path path, InvertedIndex index) throws IOException {
 		int wordCount = 0;
 
         try (BufferedReader reader = Files.newBufferedReader(path)) {
             String line;
             int position = 1;
-
+	        // TODO Stemmer stemmer = new SnowBallStemmer(...);
             while ((line = reader.readLine()) != null) {
                 String[] words = FileStemmer.parse(line);
                 wordCount += words.length;
@@ -109,14 +91,10 @@ public class FileProcessor {
                     position++;
                 }
             }
+            index.addCount(path.toString(), wordCount);
         }
-
-        if (wordCount == 0) {
-            return new InvertedIndex();
-        } else {
-            return index;
-        }
-	}
+   	}
+	
     
     /**
      * Checks if the file extension of the specified path corresponds to a text file.
