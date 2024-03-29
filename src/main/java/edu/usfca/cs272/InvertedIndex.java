d27706ce9f2d23cbcc4cb85864474fee6e97eec3
evanalulu <evanapra@gmail.com>
2024-03-29 12:48:20 -0700

    Added more has and view methods in InvertedIndex

--- src/main/java/edu/usfca/cs272/InvertedIndex.java
+++ src/main/java/edu/usfca/cs272/InvertedIndex.java
@@ -3,6 +3,10 @@ package edu.usfca.cs272;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 /**
@@ -50,48 +54,118 @@ public class InvertedIndex {
 				.add(position);
 	}
 
-	/*
-	 * TODO The get methods here are breaking encapsulation. It is now time to fix
-	 * this problem. See lecture examples linked below for how to fix this problem
-	 * efficiently.
-	 */
-
-	// TODO PrefixMap/PrefixDemo:
-	// https://usf-cs272-spring2024.notion.site/PrefixMap-and-Demo-2862dfab600341c8bfb20da697def0ce
-	// TODO WordGroup/WordPrefix:
-	// https://usf-cs272-spring2024.notion.site/WordGroupDemo-82c94c5b10c841a1a2e0df2ba1ecab37
-
 	/**
-	 * Returns the word count map.
-	 * 
-	 * @param output the count output path
+	 * Writes the word count map to a JSON file specified by the given output path.
 	 *
-	 * @throws IOException
+	 * @param output the path to the output JSON file
+	 * @throws IOException if an I/O error occurs while writing the JSON file
 	 */
 	public void writeWordCountMap(Path output) throws IOException {
 		JsonWriter.writeObject(wordCountMap, output);
 	}
 
 	/**
-	 * Returns the index map.
-	 * 
-	 * @param output the index output path
+	 * Writes the index map to a JSON file specified by the given output path.
 	 *
-	 * @throws IOException
+	 * @param output the path to the output JSON file
+	 * @throws IOException if an I/O error occurs while writing the JSON file
 	 */
 	public void writeIndexMap(Path output) throws IOException {
 		JsonWriter.writeWordPositionsMap(indexMap, output);
 	}
 
-	/*
-	 * TODO This class is still missing some methods. Use some of the class
-	 * (WordGroup/WordPrefix) and homework (FileIndex) examples to guide what you
-	 * need to add here. Some are linked below. Focus on (1) the type of methods
-	 * those examples have (add, get/view, num/size, etc.) and (2) how many. For
-	 * example, FileIndex has two "has" methods because there are two pieces of
-	 * information stored within that data structure class (the locations and the
-	 * words for a location). What does that mean for this class, which is storing
-	 * more information?
+	/**
+	 * Returns the count of words for a given document.
+	 *
+	 * @param path the document to query
+	 * @return the count of words in the document, or 0 if the document is not
+	 *   indexed
 	 */
+	public int getWordCount(Path path) {
+		return wordCountMap.getOrDefault(path.toString(), 0);
+	}
 
+	/**
+	 * Checks if a specific document is indexed in word count map.
+	 *
+	 * @param path the document to check
+	 * @return true if the document is indexed, false otherwise
+	 */
+	public boolean hasFileinCount(Path path) {
+		return wordCountMap.containsKey(path.toString());
+	}
+
+	/**
+	 * Checks if a specific word is indexed in any document.
+	 *
+	 * @param word the word to check
+	 * @return true if the word is indexed, false otherwise
+	 */
+	public boolean hasWord(String word) {
+		return indexMap.containsKey(word);
+	}
+
+	/**
+	 * Checks if the specified file is indexed in any of the word entries in index
+	 * map.
+	 *
+	 * @param file the file to check
+	 * @return true if the file is indexed, false otherwise
+	 */
+	public boolean hasFileinIndex(String file) {
+		return indexMap.values().stream().anyMatch(locationMap -> locationMap.containsKey(file));
+	}
+
+	/**
+	 * Returns the number of documents indexed.
+	 *
+	 * @return the number of indexed documents
+	 */
+	public int getFileCount() {
+		return wordCountMap.size();
+	}
+
+	/**
+	 * Returns an unmodifiable view of the set of all files (locations) indexed.
+	 *
+	 * @return an unmodifiable set of all indexed files
+	 */
+	public Set<String> viewFiles() {
+		return Collections.unmodifiableSet(wordCountMap.keySet());
+	}
+
+	/**
+	 * Returns an unmodifiable view of the set of words indexed in a specific file.
+	 *
+	 * @param file the file to query
+	 * @return an unmodifiable set of words for the specified file, or an empty set
+	 *   if the file is not indexed
+	 */
+	public Set<String> viewWordsInFile(String file) {
+		Set<String> words = new HashSet<>();
+		indexMap.forEach((word, locationMap) -> {
+			if (locationMap.containsKey(file)) {
+				words.add(word);
+			}
+		});
+		return Collections.unmodifiableSet(words);
+	}
+
+	/**
+	 * Returns an unmodifiable map of words and their positions in a specific file.
+	 *
+	 * @param file the file to query
+	 * @return an unmodifiable map of words to their positions list for the
+	 *   specified file, or an empty map if the file is not indexed
+	 */
+	public Map<String, ArrayList<Integer>> viewWordPositionsInFile(String file) {
+		Map<String, ArrayList<Integer>> wordPositions = new TreeMap<>();
+		indexMap.forEach((word, locationMap) -> {
+			ArrayList<Integer> positions = locationMap.get(file);
+			if (positions != null) {
+				wordPositions.put(word, new ArrayList<>(positions));
+			}
+		});
+		return Collections.unmodifiableMap(wordPositions);
+	}
 }
