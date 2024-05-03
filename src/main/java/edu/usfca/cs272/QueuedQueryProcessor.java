e15d60a12cb120877a91a5010cf2b828e5290919
evanalulu <evanapra@gmail.com>
2024-05-03 14:21:09 +0545

    Added basic class methods in QueuedQueryProcessor

--- src/main/java/edu/usfca/cs272/QueuedQueryProcessor.java
+++ src/main/java/edu/usfca/cs272/QueuedQueryProcessor.java
@@ -5,9 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import edu.usfca.cs272.InvertedIndex.IndexSearcher;
 import opennlp.tools.stemmer.Stemmer;
 import opennlp.tools.stemmer.snowball.SnowballStemmer;
 import opennlp.tools.stemmer.snowball.SnowballStemmer.ALGORITHM;
@@ -68,9 +72,8 @@ public class QueuedQueryProcessor {
 	/**
 	 * Processes queries stored in a file specified by the input path.
 	 *
-	 * @param path the path to the file containing query sets to be processed
-	 * @param isPartial a boolean indicating whether to use partial search (true) or
-	 *   exact search (false)
+	 * @param path the path to the file containing query sets to be processed exact
+	 *   search (false)
 	 * @throws IOException if an I/O error occurs while reading the query file or
 	 *   processing queries
 	 */
@@ -84,6 +87,97 @@ public class QueuedQueryProcessor {
 			}
 		}
 		queue.join();
+	}
+
+	/**
+	 * Retrieves the number of query lines processed by the QueuedQueryProcessor.
+	 *
+	 * @return the number of query lines processed
+	 */
+	public int numQueryLines() {
+		synchronized (searchResult) {
+			return searchResult.size();
+
+		}
+	}
+
+	/**
+	 * Retrieves the number of search results associated with the specified query.
+	 *
+	 * @param query the query for which to retrieve the number of search results
+	 * @return the number of search results for the query
+	 */
+	public int numResults(String query) {
+		synchronized (searchResult) {
+			List<IndexSearcher> results = searchResult.get(getQuerySting(query));
+			return (results != null) ? results.size() : 0;
+		}
+	}
+
+	/**
+	 * Checks if the search result map contains search results for the specified
+	 * query line.
+	 *
+	 * @param queryLine the query line to be checked
+	 * @return true if search results exist for the query line, false otherwise
+	 */
+	public boolean hasQueryLine(String queryLine) {
+		synchronized (searchResult) {
+			return searchResult.containsKey(getQuerySting(queryLine));
+		}
+	}
+
+	/**
+	 * Checks if search results exist for the specified query.
+	 *
+	 * @param query the query to be checked for search results
+	 * @return true if search results exist for the query, false otherwise
+	 */
+	public boolean hasResult(String query) {
+		String queryString = getQuerySting(query);
+		synchronized (searchResult) {
+			return searchResult.containsKey(queryString) && !searchResult.get(queryString).isEmpty();
+		}
+	}
+
+	/**
+	 * Retrieves an unmodifiable set of query strings stored in the search result
+	 * map.
+	 *
+	 * @return an unmodifiable set containing the query strings for which search
+	 *   results are stored
+	 */
+	public Set<String> viewQueries() {
+		synchronized (searchResult) {
+			return Collections.unmodifiableSet(searchResult.keySet());
+		}
+	}
+
+	/**
+	 * Retrieves an unmodifiable list of search results associated with the
+	 * specified query line.
+	 *
+	 * @param query the query line for which to retrieve search results
+	 * @return an unmodifiable list containing IndexSearcher objects representing
+	 *   the search results for the query line, or an empty list if no search
+	 *   results exist for the query line
+	 */
+	public List<IndexSearcher> viewResults(String query) {
+		synchronized (searchResult) {
+			ArrayList<IndexSearcher> searchers = searchResult.get(getQuerySting(query));
+			return (searchers != null) ? Collections.unmodifiableList(new ArrayList<>(searchers)) : Collections.emptyList();
+		}
+	}
+
+	/**
+	 * Constructs a query string by stemming the input query and joining the stemmed
+	 * words with spaces.
+	 *
+	 * @param query the query to be stemmed and joined
+	 * @return the constructed query string
+	 */
+	private static String getQuerySting(String query) {
+		return String.join(" ", FileStemmer.uniqueStems(query));
 	}
 
 	/**
