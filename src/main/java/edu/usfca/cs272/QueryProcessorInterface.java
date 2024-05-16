356444400ccd60a40e3662a7c3da41c6cc270db7
evanalulu <evanapra@gmail.com>
2024-05-17 03:06:20 +0545

    Adding remaining class methods in QueryProcessorInterface

--- src/main/java/edu/usfca/cs272/QueryProcessorInterface.java
+++ src/main/java/edu/usfca/cs272/QueryProcessorInterface.java
@@ -4,9 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
+import java.util.Set;
+
+import edu.usfca.cs272.InvertedIndex.IndexSearcher;
 
 public interface QueryProcessorInterface {
 
+	/**
+	 * Reads queries from a file and performs search on an inverted index.
+	 * 
+	 * @param path The path to the file containing queries.
+	 * @throws IOException If an I/O error occurs while reading the query file.
+	 */
 	public default void processQueries(Path path) throws IOException {
 		try (BufferedReader reader = Files.newBufferedReader(path)) {
 			String line;
@@ -17,4 +27,57 @@ public interface QueryProcessorInterface {
 	}
 
 	public abstract void processQueries(String line);
+
+	/**
+	 * Retrieves the number of query lines processed by the QueuedQueryProcessor.
+	 *
+	 * @return the number of query lines processed
+	 */
+	public abstract int numQueryLines();
+
+	/**
+	 * Retrieves the number of search results associated with the specified query.
+	 *
+	 * @param query the query for which to retrieve the number of search results
+	 * @return the number of search results for the query
+	 */
+	public abstract int numResults(String query);
+
+	/**
+	 * Checks if the search result map contains search results for the specified
+	 * query line.
+	 *
+	 * @param queryLine the query line to be checked
+	 * @return true if search results exist for the query line, false otherwise
+	 */
+	public abstract boolean hasQueryLine(String queryLine);
+
+	/**
+	 * Retrieves an unmodifiable set of query strings stored in the search result
+	 * map.
+	 *
+	 * @return an unmodifiable set containing the query strings for which search
+	 *   results are stored
+	 */
+	public abstract Set<String> viewQueries();
+
+	/**
+	 * Retrieves an unmodifiable list of search results associated with the
+	 * specified query line.
+	 *
+	 * @param query the query line for which to retrieve search results
+	 * @return an unmodifiable list containing IndexSearcher objects representing
+	 *   the search results for the query line, or an empty list if no search
+	 *   results exist for the query line
+	 */
+	public abstract List<IndexSearcher> viewResults(String query);
+
+	/**
+	 * Writes the search result map to a JSON file specified by the given output
+	 * path.
+	 *
+	 * @param output the path to the output JSON file
+	 * @throws IOException if an I/O error occurs while writing the JSON file
+	 */
+	public abstract void writeSearchResults(Path output) throws IOException;
 }
