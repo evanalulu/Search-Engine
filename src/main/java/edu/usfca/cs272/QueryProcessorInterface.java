e18bb26db12f45f908720c4b634f149b25156faf
evanalulu <evanapra@gmail.com>
2024-05-17 02:48:49 +0545

    Added processQueries in QueryProcessorInterface

--- src/main/java/edu/usfca/cs272/QueryProcessorInterface.java
+++ src/main/java/edu/usfca/cs272/QueryProcessorInterface.java
@@ -1,5 +1,20 @@
 package edu.usfca.cs272;
 
-public class QueryProcessorInterface {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
+public interface QueryProcessorInterface {
+
+	public default void processQueries(Path path) throws IOException {
+		try (BufferedReader reader = Files.newBufferedReader(path)) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				processQueries(line);
+			}
+		}
+	}
+
+	public abstract void processQueries(String line);
 }
